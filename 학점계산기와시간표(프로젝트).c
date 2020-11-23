8e0217e85d241d2ab546d8c738f6934d6736e3e9
toast-ceo <70907737+toast-ceo@users.noreply.github.com>
2020-11-24 01:49:57 +0900

    final version
    window.h를 이용한 효과추가 완료

--- 학점계산기와시간표(프로젝트).c
+++ 학점계산기와시간표(프로젝트).c
@@ -87,7 +87,7 @@ void creditcalculator() {
 	system("cls");
 
 	for (int a = 0; a < subjectNum; a++) {
-		printf("%d번째중 %d번째 과목 입력",subjectNum,a+1);
+		printf("%d번째중 %d번째 과목 입력", subjectNum, a + 1);
 		Decoration(2);
 		printf("%d번째 과목의 이름:", a + 1);
 		scanf(" %s", stu.sub[a].name);
@@ -123,7 +123,7 @@ void creditcalculator() {
 	printf("학점 총점: %.2f / 4.5\n", stu.totalAve * 1.046);
 	printf("학점 총점: %.2f / 100\n\n", stu.totalAve * 23.255);
 	Decoration(1);
-	
+
 	int arrow;
 	printf("        확인하려면 아무 숫자나 입력하시오\n                     ");
 	scanf("%d", &arrow);
@@ -168,13 +168,13 @@ float gradeReturn(char grade[]) {
 
 
 /*
-시간표 
+시간표
 입력(월, 교시, 과목, 과목아이콘)*/
 void Timetable() {
 
-	int num=5;//메뉴선택
+	int num = 5;//메뉴선택
 	struct timesubject subtime[7];// 요일, 교시
-	char uiIcon[7] = {'O','X', '=', '#', '*', 'H', 'Y' };//과목아이콘
+	char uiIcon[7] = { 'O','X', '=', '#', '*', 'H', 'Y' };//과목아이콘
 	int vis[7] = { 0 };
 
 	//초기화
@@ -190,12 +190,12 @@ void Timetable() {
 		printf("     %c      :\n", uiIcon[a]);
 	}
 	Decoration(1);
-	
-	
+
+
 	//메뉴
-	while (num !=0)
+	while (num != 0)
 	{
-	
+
 		printf("\n1.과목 등록  2.요일 입력 3.취소 4. 확인 0.종료: ");
 		scanf(" %d", &num);
 		system("cls");
@@ -213,7 +213,7 @@ void Timetable() {
 			scanf(" %c", &icontmp);
 			system("cls");
 
-		
+
 			if (uiIcon[0] == icontmp) {
 				index = 0;
 			}
@@ -252,7 +252,7 @@ void Timetable() {
 				}
 			}
 			Decoration(1);
-				
+
 		}
 		else if (num == 2) { //요일 등록
 			int index_1 = 0, index_2 = 0; char day[10], icontmp;
@@ -276,7 +276,7 @@ void Timetable() {
 			system("cls");
 
 			//확인
-			
+
 			if (day[0] == 'A') {
 				index_1 = 0;
 			}
@@ -330,39 +330,93 @@ void Timetable() {
 
 		else if (num == 3) {//취소
 
-		int cancelnum;
-		int index_1 = 0, index_2 = 0; char day1[10], icontmp1;
+			int cancelnum;
+			int index_1 = 0, index_2 = 0; char day1[10], icontmp1;
 
-		printf("1. 취소 하고 싶은 과목 2. 취소하고 싶은 요일 & 교시 시간표 아이콘\n");
-		scanf(" %d", &cancelnum);
-		system("cls");
-		
-		if (cancelnum == 1) {
-
-			Decoration(1);
-			printf("과목 아이콘 : 과목 이름\n\n");
-			for (int a = 0; a < 7; a++) {
-				if (vis[a] == 1) {
-					printf("     %c      : %s \n", uiIcon[a], subtime[a].subjectName);
-				}
-				else {
-					printf("     %c      : \n", uiIcon[a]);
-				}
-			}
-			Decoration(1);
-
-			printf("취소 하고 싶은 과목의 아이콘을 쓰시오: ");
-			scanf(" %c", &icontmp1);
+			printf("1. 취소 하고 싶은 과목 2. 취소하고 싶은 요일 & 교시 시간표 아이콘\n");
+			scanf(" %d", &cancelnum);
 			system("cls");
 
-			for (int a = 0; a < 7; a++) {
-				if (uiIcon[a] == icontmp1) {
-					strcpy(subtime[a].subjectName, " ");
+			if (cancelnum == 1) {
+
+				Decoration(1);
+				printf("과목 아이콘 : 과목 이름\n\n");
+				for (int a = 0; a < 7; a++) {
+					if (vis[a] == 1) {
+						printf("     %c      : %s \n", uiIcon[a], subtime[a].subjectName);
+					}
+					else {
+						printf("     %c      : \n", uiIcon[a]);
+					}
 				}
+				Decoration(1);
+
+				printf("취소 하고 싶은 과목의 아이콘을 쓰시오: ");
+				scanf(" %c", &icontmp1);
+				system("cls");
+
+				for (int a = 0; a < 7; a++) {
+					if (uiIcon[a] == icontmp1) {
+						strcpy(subtime[a].subjectName, " ");
+					}
+				}
+
+			}
+			else if (cancelnum == 2) {
+				ui(subtime[0].subjectIcon);
+				printf("과목 아이콘 : 과목 이름\n\n");
+				for (int a = 0; a < 7; a++) {
+					if (vis[a] == 1) {
+						printf("     %c      : %s \n", uiIcon[a], subtime[a].subjectName);
+					}
+					else {
+						printf("     %c      : \n", uiIcon[a]);
+					}
+				}
+				Decoration(1);
+				printf("취소 하고 싶은 요일과 교시를 쓰시오\n(요일은 알파벳으로 교시는 숫자로 표시하시오[ex: B4, D2 ] :  ");
+				scanf(" %s", day1);
+				system("cls");
+
+				//확인
+
+				if (day1[0] == 'A') {
+					index_1 = 0;
+				}
+				else if (day1[0] == 'B') {
+					index_1 = 1;
+				}
+				else if (day1[0] == 'C') {
+					index_1 = 2;
+				}
+				else if (day1[0] == 'D') {
+					index_1 = 3;
+				}
+				else if (day1[0] == 'F') {
+					index_1 = 4;
+				}
+				if (day1[1] == '1') {
+					index_2 = 0;
+				}
+				else if (day1[1] == '2') {
+					index_2 = 1;
+				}
+				else if (day1[1] == '3') {
+					index_2 = 2;
+				}
+				else if (day1[1] == '4') {
+					index_2 = 3;
+				}
+				else if (day1[1] == '5') {
+					index_2 = 4;
+				}
+				else if (day1[1] == '6') {
+					index_2 = 5;
+				}
+				subtime[0].subjectIcon[index_2][index_1] = ' ';
 			}
 
-		}
-		else if (cancelnum == 2) {
+			//출력
 			ui(subtime[0].subjectIcon);
 			printf("과목 아이콘 : 과목 이름\n\n");
 			for (int a = 0; a < 7; a++) {
@@ -374,76 +428,22 @@ void Timetable() {
 				}
 			}
 			Decoration(1);
-			printf("취소 하고 싶은 요일과 교시를 쓰시오\n(요일은 알파벳으로 교시는 숫자로 표시하시오[ex: B4, D2 ] :  ");
-			scanf(" %s", day1);
-			system("cls");
-
-			//확인
-
-			if (day1[0] == 'A') {
-				index_1 = 0;
-			}
-			else if (day1[0] == 'B') {
-				index_1 = 1;
-			}
-			else if (day1[0] == 'C') {
-				index_1 = 2;
-			}
-			else if (day1[0] == 'D') {
-				index_1 = 3;
-			}
-			else if (day1[0] == 'F') {
-				index_1 = 4;
-			}
-			if (day1[1] == '1') {
-				index_2 = 0;
-			}
-			else if (day1[1] == '2') {
-				index_2 = 1;
-			}
-			else if (day1[1] == '3') {
-				index_2 = 2;
-			}
-			else if (day1[1] == '4') {
-				index_2 = 3;
-			}
-			else if (day1[1] == '5') {
-				index_2 = 4;
-			}
-			else if (day1[1] == '6') {
-				index_2 = 5;
-			}
-			subtime[0].subjectIcon[index_2][index_1] = ' ';
-		}
-
-		//출력
-		ui(subtime[0].subjectIcon);
-		printf("과목 아이콘 : 과목 이름\n\n");
-		for (int a = 0; a < 7; a++) {
-			if (vis[a] == 1) {
-				printf("     %c      : %s \n", uiIcon[a], subtime[a].subjectName);
-			}
-			else {
-				printf("     %c      : \n", uiIcon[a]);
-			}
-		}
-		Decoration(1);
 
 		}
 
 		else if (num == 4) {//확인
 
-		ui(subtime[0].subjectIcon);
-		printf("과목 아이콘 : 과목 이름\n\n");
-		for (int a = 0; a < 7; a++) {
-			if (vis[a] == 1) {
-				printf("     %c      : %s \n", uiIcon[a], subtime[a].subjectName);
+			ui(subtime[0].subjectIcon);
+			printf("과목 아이콘 : 과목 이름\n\n");
+			for (int a = 0; a < 7; a++) {
+				if (vis[a] == 1) {
+					printf("     %c      : %s \n", uiIcon[a], subtime[a].subjectName);
+				}
+				else {
+					printf("     %c      : \n", uiIcon[a]);
+				}
 			}
-			else {
-				printf("     %c      : \n", uiIcon[a]);
-			}
-		}
-		Decoration(1);
+			Decoration(1);
 		}
 	}
 	system("cls");
@@ -453,11 +453,11 @@ void ui(char c[6][5]) {
 	Decoration(1);
 
 	printf("           월    화   수    목    금\n");
-	
+
 	Decoration(2);
 	//시간표 표시해주는 곳
 	for (int a = 0; a < 6; a++) {
-		printf("%d period",a+1);
+		printf("%d period", a + 1);
 		for (int b = 0; b < 5; b++) {
 			printf("  %c  |", c[a][b]);
 		}
